84eae6783878812edb5d2d5f41c6749c11617a62
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-09-22 09:00:41 -0600

    apeasement

--- lesson_01/team/team01.py
+++ lesson_01/team/team01.py
@@ -18,6 +18,7 @@ Question: if the number of threads and range_count was random, would your progra
 """
 
 from datetime import datetime, timedelta
+import math
 import threading
 import random
 
@@ -44,6 +45,18 @@ def is_prime(n):
         i += 6
     return True
 
+def do_loop(start, range_count, lock):
+    global prime_count
+    global numbers_processed
+
+    for i in range(start, start + range_count):
+        numbers_processed += 1
+        if is_prime(i):
+            with lock:
+                prime_count += 1
+                print(i, end=', ', flush=True)
+        print(flush=True)
+
 
 def main():
     global prime_count                  # Required in order to use a global variable
@@ -55,12 +68,28 @@ def main():
     start = 10000000000
     range_count = 100000
     numbers_processed = 0
-    for i in range(start, start + range_count):
-        numbers_processed += 1
-        if is_prime(i):
-            prime_count += 1
-            print(i, end=', ', flush=True)
-    print(flush=True)
+
+    thread_count = 10
+    chunk_size = range_count/thread_count
+
+    threads = [] #an empty array
+
+    # processing_lock
+
+    for i in range(1, thread_count):
+        chunk_start = math.ceil(i * chunk_size)
+        threads.push(threading.Thread(target=do_loop, args=(chunk_start, range_count)))
+        
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    # loop_doing_thread = threading.Thread(target=do_loop, args=(start, range_count))
+
+    # loop_doing_thread.start()
+    # loop_doing_thread.join()
 
     # Should find 4306 primes
     log.write(f'Numbers processed = {numbers_processed}')
